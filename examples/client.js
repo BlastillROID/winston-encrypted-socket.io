fff52ff3ecd422c9eccc2261754aaf0e26c417a1
Mounir Missaoui <mounir@fivepoints.fr>
2020-01-13 12:13:36 +0100

    initial Commit

---
+++ examples/client.js
@@ -0,0 +1,26 @@
+
+var winston = require("winston");
+require("../lib/winston-socketio");
+
+let logger = winston.createLogger({
+  level: "info",
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.SocketIO(
+      {
+        host: "http://localhost",
+        secure: true,
+        reconnect: true,
+        namespace: "log",
+        log_topic: "log"
+      }
+    )
+  ]
+});
+
+logger.log("info", "Yo Lets log to socket.io");
+logger.log("error", "Hey I logged an error");
+
+logger.level = "debug";
+
+logger.log("debug", "Some more logging....");
